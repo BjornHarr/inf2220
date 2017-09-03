f8e927237508a85504862d40f477b16a104ed312
BjornHarr <bjornharr@gmail.com>
2017-09-03 18:56:50 +0200

    Changing folder names

---
+++ oblig1/Tree.java
@@ -0,0 +1,33 @@
+public class Tree{
+    private Node root;
+
+    Tree(String element){
+        this.root = new Node(element);
+    }
+
+    public class Node{
+        private Node left;
+        private Node right;
+
+        private String element;
+
+        Node(String element){
+            this.left = null;
+            this.right = null;
+            this.element = element;
+        }
+
+        //Insert Node
+        public void add(Node newElement){
+            if (){
+
+            }
+        }
+
+
+        //Search Node
+
+
+        //Delete Node
+    }
+}
